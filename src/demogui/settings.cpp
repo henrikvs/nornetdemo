075af35fcd8e70f711bc643a6d45c23f8360f7f3
Henrik <henrikmiller@gmail.com>
2014-05-15 16:24:44 +0200

    Removed some unnecessary files

---
+++ src/demogui/settings.cpp
@@ -0,0 +1,17 @@
+#include "settings.h"
+#include <QString>
+QString Settings::sliverKey = "settings/sliverKey";
+QString Settings::sliceName = "settings/sliceName";
+QString Settings::gatekeeperKey = "settings/gatekeeperKey";
+QString Settings::gatekeeperEnabled = "settings/gatekeeperEnabled";
+QString Settings::programName = "nornetdemo";
+QString Settings::company = "Henrik Simonsen";
+QString Settings::gatekeeperUsername = "settings/gatekeeper/username";
+QString Settings::gatekeeperHostname = "settings/gatekeeper/hostname";
+
+
+QString Settings::relayEnabled = "settings/relayEnabled";
+QString Settings::relayPort = "settings/relayPort";
+QString Settings::relayHostname = "settings/relayAddress";
+SliceManager Settings::sliceManager;
+
